d6dffbc0d04098609ac05f9cb3e531917a0c3744
McWebbles <s3901416@student.rmit.edu.au>
2025-04-02 10:54:45 +1100

    added change that might fixed start up code during run time

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -212,6 +212,7 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+	delay_software_ms(100); //important
 	MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
 	configureSPIBus1();
